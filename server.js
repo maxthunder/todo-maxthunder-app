9253f38c5af29147b87ba5f95eafc849f8b1950b
maxthunder <maxwell.stark@gmail.com>
2020-03-30 14:29:36 -0600

    heroku stuff.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/todo-maxthunder-app'));
+app.use(express.static(__dirname + 'dist/todo-app'));
 app.get('/*', function(req,res){
-  res.sendFile(path.join(__dirname, 'todo-maxthunder-app', 'index.html'))
+  res.sendFile(path.join(__dirname + 'dist/todo-app/index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
