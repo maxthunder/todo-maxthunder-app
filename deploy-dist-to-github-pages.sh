22db5ee7d2d2b91bb0e00bbee04c3b5a04c06b15
maxthunder <maxwell.stark@gmail.com>
2020-03-30 23:01:22 -0600

    Updates script.

--- deploy-dist-to-github-pages.sh
+++ deploy-dist-to-github-pages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-ng build && cp -a dist/. ~/Development/deploy/maxthunder.github.io/. && ls -l ~/Development/deploy/maxthunder.github.io/.
+echo "Building app..."
+ng build
+echo "App built."
 
+echo "Copying dist files to local deploy git repo..."
+cp -a dist/todo-app/. ~/Development/deploy/maxthunder.github.io/.
+echo "Files copied."
+
+echo "Exiting..."
 exit 0;
